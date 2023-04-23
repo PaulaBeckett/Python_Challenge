2042405873b114504bbd4de60c29e5cf3473ea5c
PaulaBeckett <pollysparkles@gmail.com>
2023-04-23 16:39:12 +0930

    PyPoll Complete

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,6 +11,8 @@ list_candidates = []
 vote_candidate = [0,0,0]
 #create variable hold percentage of votes
 percent_vote = []
+#add variable to collect each candidates results
+results_list = []
 
 #open and read csv
 with open(election_file_csv, "r") as election_csv:
@@ -43,15 +45,22 @@ with open(election_file_csv, "r") as election_csv:
             #add a vote to the vote counter list
             vote_candidate[vote_index] +=1
     
+    #print total votes
+    print(f'Total Votes: {total_votes}')
+
     #run a loop to update the percentage of votes based on final tallies and total votes
     for candidate in range(3):
         #calc percentage of votes to 3 decimal places
         percent = round(((vote_candidate[candidate])/total_votes)*100,3)
         #add to percentage list
         percent_vote.append(percent)
+        #add results to list for each candidate
+        results_list.append(f"{list_candidates[candidate]}: {percent_vote[candidate]}% ({vote_candidate[candidate]})")
+        #print result list to terminal
+        print(f'{results_list[candidate]}')
 
-#print and test print proper collection of candidates, total votes and number of votes per candidate
-print(total_votes)
+#test print proper collection of candidates, total votes and number of votes per candidate
+#print(total_votes)
 #print(list_candidates)
 #print(vote_candidate)
 
@@ -62,4 +71,27 @@ most_votes = max(vote_candidate)
 #using max result find index of this in table
 winner_index = vote_candidate.index(max(vote_candidate))
 winner = list_candidates[winner_index]
-#test print(winner)
+#print winners name to terminal
+print(f'Winner: {winner}')
+
+#write results to new text file
+
+#create variable for new file
+election_results = os.path.join("Analysis", "Election_Results.txt")
+
+#open new file
+with open(election_results, "wt") as txt:
+        #define what each line should say
+        lines = ["Election Results",
+                "---------------------------------------",
+                (f'Total Votes: {total_votes}'), 
+                "---------------------------------------",
+                (f'{results_list[0]}'),
+                (f'{results_list[1]}'),
+                (f'{results_list[2]}'),
+                "---------------------------------------",
+                (f'Winner: {winner}')]   
+    #loop to write each value in the text file on its own line
+        for line in lines:
+            txt.write(line)
+            txt.write('\n')
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ The 2 below sites were used for clarification on how to write to a text file and
 https://www.w3schools.com/python/python_file_write.asp
 https://www.pythontutorial.net/python-basics/python-write-text-file
 
+PyPoll also saved along the way as steps were completed successfully
+
+
+
